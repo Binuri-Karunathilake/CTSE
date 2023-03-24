2ef41f459e42183688da9054c5a13a8dce9a5c6f
Binuri-Karunathilake <binurikarunathilake@gmail.com>
2023-03-24 15:40:41 +0530

    Create user profile

--- my-app/screens/UserProfile.js
+++ my-app/screens/UserProfile.js
@@ -1,246 +1,266 @@
-import React from 'react';
-import { Touchable, TouchableOpacity } from 'react-native';
-import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import React, {useState,useEffect} from 'react'
+import { View, Text, Image, TouchableOpacity, TextInput, ScrollView, StyleSheet } from 'react-native';
+// import * as ImagePicker from 'expo-image-picker';
+import * as ImagePicker from 'expo-image-picker';
+import {firebase}  from '../firebase'
 
-const SPACING = 20
-const AVATAR_SIZE = 70;
 
-
-const UserProfilePage = () => {
+export default Profile = () => {
   // Replace these with your own user information
-  const name = 'John Doe';
-  const email = 'john.doe@example.com';
-  const profileImageUrl = 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50';
-  const phoneNumber = '123-456-7890';
-  const address = '123 Main St, Anytown USA';
-  const bio = 'I am a software developer with a passion for building great products.';
+  const [fName,setfName] = useState ('');
+  const [Lname, setLname]= useState ('');
+  // const [phoneNumber, setPhoneNumber] = useState ('123-456-7890');
+  // const [address, setAddress] = useState ('123 Main St, Anytown USA');
+  const [email, setEmail] = useState ('');
+  const [phoneNumber,setphoneNumber] = useState ('');
+  const [image, setImage] = useState (null);
+
+  // const user = auth.currentUser;
+  // console.log(user);
+  // console.log(user.uid);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+
+  };
+
+  useEffect(() => {
+    const uid = firebase.auth().currentUser.uid;
+    firebase.firestore().collection('users').doc(uid).get()
+      .then((snapshot) => {
+        if (snapshot.exists) {
+          const { fName, Lname, email, phoneNumber, address } = snapshot.data();
+          setfName(fName);
+          setLname(Lname);
+          setEmail(email);
+          setphoneNumber(phoneNumber);
+          // setAddress(address);
+        } else {
+          console.log('User does not exist');
+        }
+      })
+      .catch((error) => {
+        console.log('Error fetching user data:', error);
+      });
+  }, []);
+
+  const handleLogout = () => {
+    firebase.auth().signOut();
+  };
 
   return (
-    <ScrollView >
-      <View style={styles.container}>
-        <View style={styles.imageView}>
-          <View style={styles.detailsContainer}>
-                  <Image
-                      source={{uri: profileImageUrl}}
-                      style={styles.image}
-                      />
-                    <View style={styles.details}>
-                      <Text style={styles.name}>{name}</Text>
-                      <Text style={styles.email}>{email}</Text>
-                    </View>
-          </View>
-          <View style={styles.headerBtn}>
-          <View>
-            <TouchableOpacity style={[styles.userBtn, styles.buttonOutline]} onpress={()=> {}}>
-                <Text style={styles.userBtnTxt}>Edit</Text>
+  <ScrollView>
+    <View style={styles.container}>
+      <View style={styles.header}></View>
+      {/* <Image
+        style={styles.avatar}
+        source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar6.png' }}
+        
+      />
+           */}
+ <TouchableOpacity style={styles.avatarContainer} onPress={pickImage}>
+          {image ? (
+            <Image source={{ uri: image }} style={styles.avatar} />
+          ) : (
+            <Image source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar6.png' }} style={styles.avatar} />
+          )}
+        </TouchableOpacity>
+
+      <View style={styles.body}>
+        <View style={styles.bodyContent}>
+          <Text style={styles.name}>{fName} {Lname}</Text>
+          {/* <Text style={styles.info}>UX Designer / Mobile developer</Text>
+          <Text style={styles.description}>
+            Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis,
+            omittam deseruisse consequuntur ius an,
+          </Text> */}
+          <View style={styles.btn}>
+            <TouchableOpacity style={[styles.buttonOutLine, styles.buttonContainer]}>
+              <Text>Edit</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.buttonOutLine, styles.logoutButtonContainer]}
+                  onPress={handleLogout}>
+              <Text>Logout</Text>
             </TouchableOpacity>
           </View>
-          <View>
-            <TouchableOpacity style={[styles.userBtn, styles.buttonOutline]} onpress={()=> {}}>
-                <Text style={styles.userBtnTxt}>Logout</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-</View>
-</View>
           <View style={styles.form}>
             <View style={styles.container1}>
-
-              <View>
-                            <Text style={styles.questions}>Phone :</Text>
-                            <View style={styles.textcontainer}>
-                            <Text style={styles.features}>{phoneNumber}</Text>
-                            </View>
-                            <Text style={styles.questions}>Address : </Text>
-                            <View style={styles.textcontainer}>
-                            <Text style={styles.paragraph}>{address}</Text>
-                            </View>
-                            <Text style={styles.questions}>Bio : </Text>
-                            <View style={styles.textcontainer}>
-                            <Text style={styles.paragraph}>{bio}</Text>
-                            </View>
-                            
-              </View>
-                        
-              {/* <View style={{flexDirection: 'row', justifyContent: 'flex-end'}}>
-                <StyleButton1 onPress={() => navigation.navigate('CoFounderRegister')}>
-                    <ButtonText>Edit</ButtonText>
-                </StyleButton1>
-                    </View> */}
-              </View>
-
-          
-          
-
+                <Text style={styles.questions}>Name : </Text>
+                <View style={styles.textcontainer}>
+                  <TextInput style={styles.paragraph} value={`${fName} ${Lname}`}/>
+                </View>
+                <Text style={styles.questions}>Email : </Text>
+                <View style={styles.textcontainer}>
+                  <TextInput style={styles.paragraph} value={email} />
+                </View>
+               <Text style={styles.questions}>Phone :</Text>
+                <View style={styles.textcontainer}>
+                  <TextInput style={styles.features} value={phoneNumber} />
+                </View>
+                {/* <Text style={styles.questions}>Address : </Text>
+                <View style={styles.textcontainer}>
+                  <TextInput style={styles.paragraph} value={address} />
+                </View>  */}
+            </View>   
           </View>
+        </View>
+      </View>
+    </View>
+  </ScrollView>
 
-     
-    </ScrollView>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    backgroundColor: '#E3E9E2',
-  },
-  detailsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    // marginBottom: SPACING/2,
-  },
-  details: {
-    flexShrink: 1,
-    width: 200
-},
-form: {
-  opacity: 5,
-  backgroundColor: '#F2F2F2',
-  padding: 10,
-  flex: 1,
-  marginTop:15
-},
-container1: {
-  backgroundColor: 'white',
-  padding: 20,
-  borderRadius: 20,
-  marginHorizontal:10
-},
-questions: {
-  fontWeight: 'bold',
-  fontSize: 18,
-  marginBottom: 8,
-  marginTop:8
-},
-textcontainer: {
-  padding: 8,
-  borderWidth: 2,
-  borderColor: '#ACADBC',
-  backgroundColor: '#ffff',
-  marginTop: 5,
-  borderRadius: 6
-},
-features: {
-  fontWeight: '500',
-  fontSize: 15,
-  marginBottom: 15,
-  color: '#1d1f1e',
-
-},
-paragraph: {
-  flex: 1,
-  marginBottom: 10,
-  // fontFamily: 'Georgia',
-  fontSize: 14,
-  lineHeight: 16 * 1.5,
-  textAlign: 'justify'
-},
-
-container2: {
-  backgroundColor: '#E3E9E2',
-  borderTopRightRadius: 20
-},
-
-  // body: {
-  //   borderTopLeftRadius : 50,
-  //   borderTopRightRadius : 50,
-  //   padding: 16,
-  //   backgroundColor: 'red',
-
-  // },
   header: {
-    alignItems: 'center',
-    // paddingVertical: 16,
-    backgroundColor: '#f1f1f1',
-    
+    backgroundColor: '#10B981',
+    height: 200,
   },
-  headerBtn: {
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: 'white',
+    marginBottom: 10,
+    alignSelf: 'center',
+    position: 'absolute',
+    marginTop: 130,
+  },
+
+  avatarContainer: {
+    justifyContent: 'center',
     alignItems: 'center',
-    paddingLeft: 250,
+  },
+  addImage:{
+    flexDirection: 'column',
+    marginTop: 120,
+    marginRight:40,
+    justifyContent: 'space-evenly',
+    alignItems: 'flex-end'
+  },
+  // name: {
+  //   fontSize: 22,
+  //   color: '#FFFFFF',
+  //   fontWeight: '600',
+  // },
+  body: {
+    marginTop: 40,
+  },
+  bodyContent: {
+    alignItems: 'center',
+    padding: 30,
+  },
+  name: {
+    fontSize: 28,
+    color: '#696969',
+    fontWeight: '600',
+  },
+  info: {
+    fontSize: 16,
+    color: '#00BFFF',
+    marginTop: 10,
+  },
+  description: {
+    fontSize: 16,
+    color: '#696969',
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  buttonContainer: {
+    marginTop: 10,
+    height: 45,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    marginRight: 20,
+    marginLeft: 20,
+    width: 150,
+    borderRadius: 10,
+    
+    // backgroundColor: '#10B981',
+  },
+  logoutButtonContainer: {
+    marginTop: 10,
+    height: 45,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    marginRight: 20,
+    marginLeft: 20,
+    width: 150,
+    borderRadius: 10,
+    backgroundColor: '#10B981',
+  },
+
+  btn: {
+    alignItems: 'center',
+    // paddingLeft: 250,
     // paddingHorizontal: 120,
     // backgroundColor: '#f1f1f1',
     display:'flex' ,
     flexDirection: 'row',
-    justifyContent: 'space-evenly',   
+    justifyContent: 'space-evenly', 
   },
-  profileImage: {
-    width: 120,
-    height: 120,
-    borderRadius: 60,
-  },
-  name: {
-    marginTop: 8,
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  email: {
-    marginTop: 8,
-    fontSize: 18,
-    color: '#666',
-  },
-
-  infoContainer: {
-
-    flexDirection: 'row',
-    // marginBottom: 16,
-  },
-  label: {
-    fontWeight: 'bold',
-    marginRight: 8,
-  },
-  value: {
-    // flex: 1,
-  },
-
-  userBtn: {
-        backgroundColor: '#0782F9',
-        width: '100%',
-        padding: 10,
-        borderRadius: 5,
-        // alignItems: 'center',
-    },
-
-    buttonOutline : {
-        backgroundColor: 'white',
+  buttonOutLine: {
+    backgroundColor: 'white',
         marginTop: 5,
-        borderColor: '#0782F9',
+        borderColor: '#10B981',
         borderWidth: 2,
-    },
-
-    buttonOutlineText : {
-        color: '#0782F9',
-        fontWeight: '700',
-        fontSize: 12
-    },
-    
-    userBtnTxt : {
-        color: 'black',
-        fontWeight: '700',
-        fontSize: 12
-    },
-
-    imageView: {
-      padding: SPACING,
-      // marginTop: 30,
-      backgroundColor: 'rgba(255, 255, 255, 0.0)',
-      borderRadius: 12,
-      // marginBottom: SPACING,
-      shadowColor: 'black',
-      shadowOffset: {
-          width: 0,
-          height: 10
-    },
-      shadowOpacity: 1,
-      shadowRadius: 20,
   },
-  image: {
-    width: 120,
-    height: 120,
-    borderRadius: AVATAR_SIZE,
-    backgroundColor: 'red',
-    marginEnd: 20
-},
+  form: {
+    opacity: 5,
+    backgroundColor: '#F2F2F2',
+    padding: 5,
+    marginBottom:5,
+    width: '100%'
 
-});
+  },
+  container1: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 20,
+  },
+  questions: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 8,
+    marginTop:8
+  },
+  textcontainer: {
+    padding: 8,
+    borderWidth: 2,
+    borderColor: '#ACADBC',
+    backgroundColor: '#ffff',
+    marginTop: 5,
+    borderRadius: 6
+  },
+  features: {
+    fontWeight: '500',
+    fontSize: 15,
+    marginBottom: 15,
+    color: '#1d1f1e',
+  
+  },
+  paragraph: {
+    marginBottom: 10,
+    // fontFamily: 'Georgia',
+    fontSize: 14,
+    lineHeight: 16 * 1.5,
+    textAlign: 'justify',
+    maxWidth: 200,
+    minWidth: 500
+  },
 
-export default UserProfilePage;
+
+})
