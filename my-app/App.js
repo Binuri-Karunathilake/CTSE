1c82398a47569c7548f9d9bdfee948cfe32ca9c7
Binuri-Karunathilake <binurikarunathilake@gmail.com>
2023-03-24 12:49:02 +0530

    Made changes

--- my-app/App.js
+++ my-app/App.js
@@ -1,25 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from "react";
+import { firebase } from "./firebase";
+
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 import UserProfilePage from './screens/UserProfile';
+import AdminHomeScreen from './screens/AdminHomeScreen';
+import EditProfile from './screens/EditProfile';
+import Login from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 
-export default function App() {
+function App() {
+  const [initilizing, setInitilizing] = useState(true);
+  const [user, setUser] = useState();
+
+  //handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initilizing) setInitilizing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+  if (initilizing) return null;
+
+  if(!user) {
+
   return (
-    <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen name='UserProfile' component={UserProfilePage} />
         <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
         
       </Stack.Navigator>
-    </NavigationContainer>
   );
 }
 
+return(
+  <Stack.Navigator>
+    <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Screen name="AdminHome" component={AdminHomeScreen} />        
+    <Stack.Screen options={{headerShown: false}} name='EditProfile' component={EditProfile} />
+  </Stack.Navigator>
+);
+}
+
+export default () =>{
+  return(
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  )
+}
 const Stack = createNativeStackNavigator();
 
 const styles = StyleSheet.create({
