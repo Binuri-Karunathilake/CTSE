8b4800dd96c9d411a9aef270517979092bf5907f
Adeesha <86665415+it20020644@users.noreply.github.com>
2023-03-25 21:17:38 +0530

    djbddabjdja

--- my-app/screens/ShoppingCart.js
+++ my-app/screens/ShoppingCart.js
@@ -7,6 +7,10 @@ const ShoppingCartScreen = () => {
     { id: '1', name: 'Item 1', price: 10, quantity: 1 },
     { id: '2', name: 'Item 2', price: 20, quantity: 1 },
     { id: '3', name: 'Item 3', price: 30, quantity: 1 },
+    { id: '1', name: 'Item 1', price: 10, quantity: 1 },
+    { id: '2', name: 'Item 2', price: 20, quantity: 1 },
+    { id: '3', name: 'Item 3', price: 30, quantity: 1 },
+
   ]);
 
   const handleUpdateItem = (itemId, newQuantity) => {
