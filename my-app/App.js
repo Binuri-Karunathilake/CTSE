63799dd65a5989763f5c999daecf7e32a90df1a4
Adeesha <86665415+it20020644@users.noreply.github.com>
2023-03-26 00:02:13 +0530

    huhi

--- my-app/App.js
+++ my-app/App.js
@@ -29,7 +29,6 @@ const Stack = createNativeStackNavigator();
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
